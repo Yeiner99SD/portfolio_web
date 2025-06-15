acc1842c0878a242fd7126f2cd7c1b5fdfa76553
Yeiner99SD <yeinermoreno2005@gmail.com>
2025-06-15 16:25:05 -0500

    Creacion del proyecto con React y talwind css en la rama master

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className="min-h-screen flex items-center justify-center bg-red-500  text-white">
+          <h1 className="text-4xl">Portfolio</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
